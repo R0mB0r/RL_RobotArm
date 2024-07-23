1d569e5d2caa8a2b2d70bc024690f36f32df4c2d
YoshidaLab <romain.bornier@ensta-bretagne.org>
2024-07-23 15:04:38 +0900

    230724 15:00

--- Xarm6/xarm6_mujoco/envs/force.py
+++ Xarm6/xarm6_mujoco/envs/force.py
@@ -6,83 +6,39 @@ import mujoco as mj
 import matplotlib.pyplot as plt
 
 def get_sensor_data(data, sensor_name: str) -> np.ndarray:
-    """
-    Get the sensor data from the environment.
-
-    Parameters:
-    - data: The MuJoCo data object.
-    - sensor_name: The name of the sensor.
-
-    Returns:
-    - np.ndarray: The sensor data from the environment.
-    """
-    return data.sensor("contact_force").data
+    return data.sensor(sensor_name).data
 
 def get_force_sensor_data(data) -> np.ndarray:
-    """
-    Get the force sensor data from the environment.
-
-    Parameters:
-    - data: The MuJoCo data object.
-
-    Returns:
-    - np.ndarray: The force sensor data from the environment.
-    """
     return get_sensor_data(data, "contact_force")
 
-# Path to the MuJoCo model file for the reach environment
 MODEL_XML_PATH = os.path.join(os.path.dirname(__file__), "../assets/", "force.xml")
 
 class Xarm6ForceEnv(Xarm6):
-    def __init__(self, distance_threshold: float = 0.02, goal_force=np.array([0,0,40]), **kwargs: Any):
+    def __init__(self, distance_threshold: float = 0.02, goal_force=np.array([0, 0, 40]), **kwargs: Any):
         self.model_path = MODEL_XML_PATH
         self.distance_threshold = distance_threshold
         self.goal_force = goal_force
-        
-        # Initialize parent class with model path and environment settings
-        super().__init__(
-            model_path=self.model_path,
-            n_substeps=20,
-            block_gripper=True,
-            **kwargs,
-        )
+        super().__init__(model_path=self.model_path, n_substeps=20, block_gripper=True, **kwargs)
 
-        self.nu = self.model.nu  # Number of actuators
-        self.nq = self.model.nq
-        self.nv = self.model.nv
+        self.num_actuators = self.model.nu
+        self.num_joints = self.model.nq
+        self.num_velocities = self.model.nv
         self.ctrl_range = self.model.actuator_ctrlrange
 
-        # Initialize variables for real-time plotting
         self.fig, (self.ax1, self.ax2, self.ax3) = plt.subplots(3, 1)
         self.distance_data = []
         self.force_data = []
-        self.force_treshold = 5.0
+        self.force_threshold = 5.0
         self.speed_data = []
 
-
-    def _env_setup(self, neutral_joint_values: np.ndarray) -> None:
-        """
-        Set up the environment to a neutral pose.
-
-        Parameters:
-        - neutral_joint_values (np.ndarray): Neutral positions for the robot joints.
-        """
+    def _env_setup(self, neutral_joint_positions: np.ndarray) -> None:
         self.set_joint_neutral()
-        self.data.ctrl[0:7] = neutral_joint_values[0:7]
+        self.data.ctrl[0:7] = neutral_joint_positions[0:7]
         mj.mj_forward(self.model, self.data)
         self.grasp_site_pose = self.get_ee_orientation().copy()
         self._mujoco_step()
 
     def step(self, action: np.ndarray) -> tuple:
-        """
-        Execute a step in the environment.
-        
-        Parameters:
-        - action (np.ndarray): The action to be applied.
-
-        Returns:
-        - tuple: Observation, reward, termination flag, and additional info.
-        """
         if np.array(action).shape != self.action_space.shape:
             raise ValueError("Action dimension mismatch")
 
@@ -97,100 +53,72 @@ class Xarm6ForceEnv(Xarm6):
         info = {"is_success": self._is_success(obs["achieved_goal"], obs["desired_goal"])}
         truncated = self.compute_truncated(obs["achieved_goal"], self.goal, info)
         terminated = bool(info["is_success"])
-        reward = self.compute_reward(obs["observation"],obs["achieved_goal"], obs["desired_goal"])
+        reward = self.compute_reward(obs["observation"], obs["achieved_goal"], obs["desired_goal"])
 
-        # Update real-time plot
-        self.update_plot()
+        # self.update_plot()
 
+        return obs, reward, terminated, truncated, info
 
-        return obs, reward,terminated, truncated, info
-    
     def _get_obs(self) -> dict:
-        """
-        Get the current observation of the environment.
-
-        Returns:
-        - dict: Current state observation including position and velocity of the end-effector.
-        """
         ee_position = self._utils.get_site_xpos(self.model, self.data, "ee_center_site").copy()
         ee_velocity = self._utils.get_site_xvelp(self.model, self.data, "ee_center_site").copy() * self.dt
         ee_force = get_force_sensor_data(self.data)
 
         return {
             "observation": np.concatenate([ee_position, ee_velocity, ee_force]),
-            "achieved_goal": np.concatenate([ee_position, ee_force]),  
-            "desired_goal": np.concatenate([self.goal,self.goal_force]),
+            "achieved_goal": np.concatenate([ee_position, ee_force]),
+            "desired_goal": np.concatenate([self.goal, self.goal_force]),
         }
-    
-    def compute_reward(self,observation:np.ndarray, achieved_goal: np.ndarray, desired_goal: np.ndarray, alpha=10, beta=0.0004, gamma=1.) -> float:
+
+    def compute_reward(self, observation: np.ndarray, achieved_goal: np.ndarray, desired_goal: np.ndarray, alpha=100, beta=0.0004, gamma=100) -> float:
+        offset = 0.0013198
         ee_position = achieved_goal[0:3]
         goal_position = desired_goal[0:3]
         
         distance_to_goal = np.linalg.norm(ee_position - goal_position)
-        
         is_reached = distance_to_goal < self.distance_threshold
         is_contact = np.linalg.norm(achieved_goal[3:6]) > 0
 
         ee_speed = observation[3:6]
-        ee_speed_error = np.linalg.norm(ee_speed) 
-
+        ee_speed_error = np.linalg.norm(ee_speed) - offset
 
         if is_reached and is_contact:
-            print ("Reached and contact")
             ee_force = achieved_goal[3:6]
             goal_force = desired_goal[3:6]
             force_error = np.linalg.norm(ee_force - goal_force)
-            
-            reward = - beta * force_error - gamma * ee_speed_error
-
+            reward = -beta * force_error - gamma * ee_speed_error
         else:
-            reward = - alpha * distance_to_goal
+            reward = -alpha * distance_to_goal
         
         self.distance_data.append(distance_to_goal)
         self.force_data.append(achieved_goal[3:6])
         self.speed_data.append(ee_speed_error)
 
         return reward
-    
-    def _is_success(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> np.float32:
-        
+
+    def _is_success(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> bool:
         return False
 
     def _render_callback(self) -> None:
-        """
-        Render the environment.
-
-        Updates the target site position for visualization.
-        """
         sites_offset = (self.data.site_xpos - self.model.site_pos).copy()
         site_id = self._model_names.site_name2id["target"]
         self.model.site_pos[site_id] = self.goal - sites_offset[site_id]
         self._mujoco.mj_forward(self.model, self.data)
 
     def _sample_goal(self) -> np.ndarray:
-        """
-        Sample a target goal for the environment.
-
-        Returns:
-        - np.ndarray: The sampled target goal.
-        """
-        goal = self._utils.get_site_xpos(self.model, self.data, "target").copy()
-        return goal
+        return self._utils.get_site_xpos(self.model, self.data, "target").copy()
     
     def update_plot(self) -> None:
-        """
-        Update the real-time plot of force sensor data and distance to the goal.
-        """
         if len(self.force_data) > 0:
             force_data_np = np.array(self.force_data)
             x_vals = np.arange(len(force_data_np))
 
             self.ax1.clear()
-            if force_data_np.shape[1] == 3:  # Assuming force data is in 3D
+            if force_data_np.shape[1] == 3:
                 self.ax1.plot(x_vals, force_data_np[:, 0], label='Force X', color='red')
                 self.ax1.plot(x_vals, force_data_np[:, 1], label='Force Y', color='green')
                 self.ax1.plot(x_vals, force_data_np[:, 2], label='Force Z', color='blue')
-                self.ax1.plot(x_vals, self.goal_force[2]*np.ones(len(force_data_np)), label='Goal Force Z', linestyle='--', color='darkblue')
+                self.ax1.plot(x_vals, self.goal_force[2] * np.ones(len(force_data_np)), label='Goal Force Z', linestyle='--', color='darkblue')
             self.ax1.set_xlabel('Time Step')
             self.ax1.set_ylabel('Force')
             self.ax1.set_title('Force Sensor Data')
@@ -202,7 +130,7 @@ class Xarm6ForceEnv(Xarm6):
 
             self.ax2.clear()
             self.ax2.plot(x_vals, distance_vals, label='Distance to Goal', color='red')
-            self.ax2.plot(x_vals, self.distance_threshold*np.ones(len(force_data_np)), label='Threshold', linestyle='--', color='green')
+            self.ax2.plot(x_vals, self.distance_threshold * np.ones(len(force_data_np)), label='Threshold', linestyle='--', color='green')
             self.ax2.set_xlabel('Time Step')
             self.ax2.set_ylabel('Distance')
             self.ax2.set_title('Distance to Goal')
@@ -213,14 +141,10 @@ class Xarm6ForceEnv(Xarm6):
             x_vals = np.arange(len(speed_vals))
 
             self.ax3.clear()
-            self.ax3.plot(x_vals, speed_vals, label='EE_speed', color='red')
+            self.ax3.plot(x_vals, speed_vals, label='EE Speed', color='red')
             self.ax3.set_xlabel('Time Step')
             self.ax3.set_ylabel('Speed')
             self.ax3.set_title('End-effector Speed')
             self.ax3.legend()
-    
-
         
-
-        self.fig.canvas.draw()
         plt.pause(0.001)
--- Xarm6/xarm6_mujoco/envs/reach.py
+++ Xarm6/xarm6_mujoco/envs/reach.py
@@ -4,8 +4,6 @@ from typing import Any, SupportsFloat
 from xarm6_mujoco.envs.xarm6_env import Xarm6
 import pygame
 
-
-# Path to the MuJoCo model file for the reach environment
 MODEL_XML_PATH = os.path.join(os.path.dirname(__file__), "../assets/", "reach.xml")
 
 class Xarm6ReachEnv(Xarm6):
@@ -17,23 +15,12 @@ class Xarm6ReachEnv(Xarm6):
         goal_z_range: float = 0.3,
         **kwargs: Any,
     ):
-        """
-        Initialize the Xarm6 Reach environment.
-
-        Parameters:
-        - distance_threshold (float): Distance within which the goal is considered achieved.
-        - goal_xy_range (float): Range for the goal position in the XY plane.
-        - goal_x_offset (float): Offset in the X direction for goal positioning.
-        - goal_z_range (float): Range for the goal position in the Z direction.
-        - kwargs: Additional arguments passed to the parent class.
-        """
         self.model_path = MODEL_XML_PATH
         self.distance_threshold = distance_threshold
         self.goal_xy_range = goal_xy_range
         self.goal_x_offset = goal_x_offset
         self.goal_z_range = goal_z_range
 
-        # Define goal positioning bounds
         self.goal_range_low = np.array([
             -self.goal_xy_range / 2 + self.goal_x_offset, 
             -self.goal_xy_range / 2, 
@@ -46,7 +33,6 @@ class Xarm6ReachEnv(Xarm6):
             self.goal_z_range
         ]) + np.array([0.6, 0, 0])
 
-        # Initialize parent class with model path and environment settings
         super().__init__(
             model_path=self.model_path,
             n_substeps=20,
@@ -54,19 +40,12 @@ class Xarm6ReachEnv(Xarm6):
             **kwargs,
         )
 
-        self.nu = self.model.nu  # Number of actuators
-        self.nq = self.model.nq  # Number of positions
-        self.nv = self.model.nv  # Number of velocities
-        self.ctrl_range = self.model.actuator_ctrlrange  # Control range for actuators
-
+        self.num_actuators = self.model.nu
+        self.num_positions = self.model.nq
+        self.num_velocities = self.model.nv
+        self.control_range = self.model.actuator_ctrlrange
 
     def _env_setup(self, neutral_joint_values: np.ndarray) -> None:
-        """
-        Set up the environment to a neutral pose.
-
-        Parameters:
-        - neutral_joint_values (np.ndarray): Neutral positions for the robot joints.
-        """
         self.set_joint_neutral()
         self.data.ctrl[0:7] = neutral_joint_values[0:7]
         self._mujoco.mj_forward(self.model, self.data)
@@ -74,17 +53,7 @@ class Xarm6ReachEnv(Xarm6):
         self._mujoco_step()
 
     def step(self, action: np.ndarray) -> tuple:
-        """
-        Execute a step in the environment.
-
-        Parameters:
-        - action (np.ndarray): The action to be applied.
-
-        Returns:
-        - tuple: Observation, reward, termination flag, truncation flag, and additional info.
-        """
-
-        if np.array(action).shape != self.action_space.shape:
+        if action.shape != self.action_space.shape:
             raise ValueError("Action dimension mismatch")
 
         self._set_action(action)
@@ -94,35 +63,19 @@ class Xarm6ReachEnv(Xarm6):
         if self.render_mode == "human":
             self.render()
 
-        obs = self._get_obs().copy()
-        info = {"is_success": self._is_success(obs["achieved_goal"], self.goal)}
+        observation = self._get_obs().copy()
+        info = {"is_success": self._is_success(observation["achieved_goal"], self.goal)}
         terminated = bool(info["is_success"])
-        truncated = self.compute_truncated(obs["achieved_goal"], self.goal, info)
-        reward = self.compute_reward(obs["achieved_goal"], self.goal, info)
+        truncated = self.compute_truncated(observation["achieved_goal"], self.goal, info)
+        reward = self.compute_reward(observation["achieved_goal"], self.goal, info)
 
-        return obs, reward, terminated, truncated, info
+        return observation, reward, terminated, truncated, info
 
     def _is_success(self, achieved_position: np.ndarray, desired_goal: np.ndarray) -> np.float32:
-        """
-        Determine if the goal is achieved.
-
-        Parameters:
-        - achieved_position (np.ndarray): Current position of the end-effector.
-        - desired_goal (np.ndarray): Target goal position.
-
-        Returns:
-        - np.float32: Indicates if the goal is achieved (1) or not (0).
-        """
-        d = self.goal_distance(achieved_position, desired_goal)
-        return (d < self.distance_threshold).astype(np.float32)
+        distance = self.goal_distance(achieved_position, desired_goal)
+        return (distance < self.distance_threshold).astype(np.float32)
 
     def _get_obs(self) -> dict:
-        """
-        Get the current observation of the environment.
-
-        Returns:
-        - dict: Current state observation including position and velocity of the end-effector.
-        """
         ee_position = self._utils.get_site_xpos(self.model, self.data, "ee_center_site").copy()
         ee_velocity = self._utils.get_site_xvelp(self.model, self.data, "ee_center_site").copy() * self.dt
 
@@ -133,77 +86,21 @@ class Xarm6ReachEnv(Xarm6):
         }
 
     def compute_reward(self, achieved_goal: np.ndarray, desired_goal: np.ndarray, info: dict) -> SupportsFloat:
-        """
-        Calculate the reward for the current state.
-
-        Parameters:
-        - achieved_goal (np.ndarray): Position of the achieved goal.
-        - desired_goal (np.ndarray): Target goal position.
-        - info (dict): Additional information.
-
-        Returns:
-        - SupportsFloat: The computed reward based on distance to the goal.
-        """
-        d = self.goal_distance(achieved_goal, desired_goal)
-        return -d
+        distance = self.goal_distance(achieved_goal, desired_goal)
+        return -distance
 
     def _render_callback(self) -> None:
-        """
-        Render the environment.
-
-        Updates the target site position for visualization.
-        """
         sites_offset = (self.data.site_xpos - self.model.site_pos).copy()
         site_id = self._model_names.site_name2id["target"]
         self.model.site_pos[site_id] = self.goal - sites_offset[site_id]
         self._mujoco.mj_forward(self.model, self.data)
 
     def _sample_goal(self) -> np.ndarray:
-        """
-        Sample a new goal position within the specified range.
-
-        Returns:
-        - np.ndarray: Sampled goal position.
-        """
         goal = np.array([0.0, 0.0, 0.05])
         noise = self.np_random.uniform(self.goal_range_low, self.goal_range_high)
         goal += noise
         return goal
-    
-    def modify_goal_position(self, key):
-        step_size = 0.01  # Ajustez cette valeur selon vos besoins
-        current_goal = self.goal
-        if pygame.K_w in key:    # Augmente la coordonnée z
-            current_goal[2] += step_size
-        elif pygame.K_s in key:  # Diminue la coordonnée z
-            current_goal[2] -= step_size
-        elif pygame.K_a in key:  # Diminue la coordonnée x
-            current_goal[0] -= step_size
-        elif pygame.K_d in key:  # Augmente la coordonnée x
-            current_goal[0] += step_size
-        elif pygame.K_q in key:  # Diminue la coordonnée y
-            current_goal[1] -= step_size
-        elif pygame.K_e in key:  # Augmente la coordonnée y
-            current_goal[1] += step_size
-
-        # Limiter la position du goal à l'intérieur de la plage spécifiée
-        current_goal[0] = np.clip(current_goal[0], self.goal_range_low[0], self.goal_range_high[0])
-        current_goal[1] = np.clip(current_goal[1], self.goal_range_low[1], self.goal_range_high[1])
-        current_goal[2] = np.clip(current_goal[2], self.goal_range_low[2], self.goal_range_high[2])
-        self.goal = current_goal
 
     def goal_distance(self, goal_a: np.ndarray, goal_b: np.ndarray) -> SupportsFloat:
-        """
-        Compute the distance between two goal positions.
-
-        Parameters:
-        - goal_a (np.ndarray): First goal position.
-        - goal_b (np.ndarray): Second goal position.
-
-        Returns:
-        - SupportsFloat: Euclidean distance between the two goals.
-        """
         assert goal_a.shape == goal_b.shape
         return np.linalg.norm(goal_a - goal_b)
-
-
